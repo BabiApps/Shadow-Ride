4b4024cab927266d83b1f5a110fce82d28ee9326
3LD0$ <eldar16sh@gmail.con>
2021-09-08 23:35:15 +0300

    Update fun.js
    - Add test print

--- fun.js
+++ fun.js
@@ -1,6 +1,6 @@
 //מקבלת שתי תחנות\מיקומים על מסלול האוטובוס
 //מחזירה את ההפרש המעלות בין האוטובוס לשמש, ובאיזה צד השמש
 function per2stops(startStop, endStop){
-
+    console.log('test');
     return {celius:"",side:""};
 }
