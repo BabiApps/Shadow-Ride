15deb2c0c990785c7cd9f6b8db0790c50e527fac
3LD0$ <eldar16sh@gmail.con>
2021-09-08 23:43:44 +0300

    Update fun.js
    - Showing to Shilix

--- fun.js
+++ fun.js
@@ -2,5 +2,6 @@
 //מחזירה את ההפרש המעלות בין האוטובוס לשמש, ובאיזה צד השמש
 function per2stops(startStop, endStop){
     console.log('test');
+    console.log('eldar hagever');
     return {celius:"",side:""};
 }
