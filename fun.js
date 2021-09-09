b84e7df0f3cd052107120a99fca71efdcca6ad4b
sHiLo BaBilA <shilobabila1@gmail.com>
2021-09-10 02:55:07 +0300

    Update fun.js

--- fun.js
+++ fun.js
@@ -1,6 +1,10 @@
-//מקבלת שתי תחנות\מיקומים על מסלול האוטובוס
-//מחזירה את ההפרש המעלות בין האוטובוס לשמש, ובאיזה צד השמש
-function per2stops(startStop, endStop){
+/*
+* Get: two stops (close to each other) on a bus line.
+* Return: the remainder between bus and sun, and on which side the sun.
+*
+* Future: receive time
+*/
+function per2stops(startStop, endStop){ 
     
     //Find the celuis of the bus to Y-axis
     var cBus;
@@ -8,7 +12,7 @@ function per2stops(startStop, endStop){
     yDIff = endStop.y - startStop.y; //.y is Latitude
 
     if(yDIff==0&&xDiff==0){
-        //ERROR: Same location!!
+        //ERROR: Same location!! TODO:------------
     }
     // Math.atan2 return value in radians.
     cBus = Math.atan2(yDIff,xDiff) * 180 / Math.PI;
@@ -16,6 +20,8 @@ function per2stops(startStop, endStop){
     cBus = xAngle_to_yAngle(cBus);
 
 
+
+    //for checking...
     var a='lo_shave_whatsapp';
     if (a == "whatsapp")
         a='no problom';
@@ -24,17 +30,16 @@ function per2stops(startStop, endStop){
     return {celius:a,side:cBus};
 }
 
+
+
 /*
 * Get the angle (c) between bus and the X-axis
 * Return the angle (c) between bus and the Y-axis
 */
 function xAngle_to_yAngle(xAngle){
-
-    if (xAngle=>180){
-        yAngle=-(xAngle-90);
-    }
-    else{
-        yAngle=90+(-xAngle);
-    }
+    if (xAngle=>180)
+    { yAngle=-(xAngle-90); }
+    else
+    { yAngle=90+(-xAngle); }
     return yAngle;
 }
